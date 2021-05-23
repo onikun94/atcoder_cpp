b4bc23b6dafca54d3aa4bb38cfda63f89553b426
onikun94 <onikun94@gmail.com>
2021-05-23 21:15:53 +0900

    abc202

---
+++ abc202/c/main.cpp
@@ -0,0 +1,35 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int N;
+    cin >> N;
+    vector<int> A(N), B(N), C(N);
+    for (int i = 0; i < N; i++)
+    {
+        cin >> A[i];
+    }
+    for (int i = 0; i < N; i++)
+    {
+        cin >> B[i];
+    }
+    for (int i = 0; i < N; i++)
+    {
+        cin >> C[i];
+    }
+
+    int count = 0;
+    for (int i = 0; i < N; i++)
+    {
+        for (int j = 0; j < N; j++)
+        {
+            if (A[i] == B[C[j] - 1])
+            {
+                count++;
+            }
+        }
+    }
+    cout << count << endl;
+    return 0;
+}
